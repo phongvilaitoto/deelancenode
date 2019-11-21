a7e05f1706224a988eb8d8fe19b860665d8d4b40
phongvilaitoto <phongvilaitoto@gmail.com>
2019-11-21 13:17:02 +0700

    naja

--- src/plugins/mongodb/mongodb.js
+++ src/plugins/mongodb/mongodb.js
@@ -1,14 +1,14 @@
 const mongoose = require('mongoose') // require mongoose
-const url = 'mongodb://127.0.0.1:27017/deelance'
+const uri = 'mongodb://127.0.0.1:27017/deelance'
 
 const connect = async () => {
     try {
-        await mongoose.connect(url, { //Connect to mongodb
+        await mongoose.connect(uri, { //Connect to mongodb
             useNewUrlParser: true,
             useCreateIndex: true,
             useUnifiedTopology: true
         })
-        console.log('Connected to', url)
+        console.log('Connected to', uri)
     } catch(err) {
         throw new Error(err)
     }
