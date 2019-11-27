1dca9dbb3ebc97fb6d1398c3445f24b7ee8531cb
phongvilaitoto <phongvilaitoto@gmail.com>
2019-11-27 17:05:44 +0700

    tt

--- public/js/chunk-2d230447.958073d5.js
+++
@@ -1,2 +0,0 @@
-(window["webpackJsonp"]=window["webpackJsonp"]||[]).push([["chunk-2d230447"],{ec0c:function(e,n,t){"use strict";t.r(n);var c=function(){var e=this,n=e.$createElement;e._self._c;return e._m(0)},r=[function(){var e=this,n=e.$createElement,t=e._self._c||n;return t("div",[t("h1",[e._v("Feedback")])])}],u={},a=u,s=t("2877"),i=Object(s["a"])(a,c,r,!1,null,"2b65f197",null);n["default"]=i.exports}}]);
-//# sourceMappingURL=chunk-2d230447.958073d5.js.map
